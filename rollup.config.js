a749816f5d829837d0aced6c243a88802f7472d7
Chris Nolan <chris@leytech.co.uk>
2022-07-14 11:07:16 +0100

    Build additional files with no version in filename so these are always latest release

--- rollup.config.js
+++ rollup.config.js
@@ -17,15 +17,21 @@ export default [
     },
     {
         input: "src/GetAddress.ts",
-        output: 
+        output: [
             {
                 file:"dist/getaddress-find-" + version + ".js",
                 format:"iife", 
                 name:'getAddress',
                 sourcemap:  "inline"
+            },
+            {
+                file:"dist/getaddress-find.js",
+                format:"iife",
+                name:'getAddress',
+                sourcemap:  "inline"
             }
-        
-        ,plugins:[nodeResolve(),ts(
+        ],
+        plugins:[nodeResolve(),ts(
             {tsconfig: {
                 declaration: false
             }}
@@ -33,12 +39,18 @@ export default [
     },
     {
         input: "dist/getaddress-find.mjs",
-        output: 
+        output: [
             {
                 file:"dist/getaddress-find-" + version + ".min.js",
                 format:"iife",
                 name:'getAddress'
             },
+            {
+                file:"dist/getaddress-find.min.js",
+                format:"iife",
+                name:'getAddress'
+            }
+        ],
         plugins:[terser()]
     }
 ]
